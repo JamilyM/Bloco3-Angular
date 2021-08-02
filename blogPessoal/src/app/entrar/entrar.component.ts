8df83b43570f290163a199562ea75735d4b09b0c
JamiyM <jamilymelo787@gmail.com>
2021-08-02 13:41:52 -0300

    Componente inicio sem consumo

--- blogPessoal/src/app/entrar/entrar.component.ts
+++ blogPessoal/src/app/entrar/entrar.component.ts
@@ -31,10 +31,6 @@ export class EntrarComponent implements OnInit {
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
 
-      console.log(environment.token)
-      console.log(environment.foto)
-      console.log(environment.nome)
-      
       this.router.navigate(['/inicio'])
     }, erro =>{
       if(erro.status == 500){
